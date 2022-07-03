9ea95e0330b66eb748ae28d40659e3da8103f0cb
bluusocks <nix.jakob@gmail.com>
2022-07-03 15:36:31 -0400

    Update kimonoya-japan.py
    started adding simple gui

--- kimonoya-japan.py
+++ kimonoya-japan.py
@@ -1,5 +1,6 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
+import PySimpleGUI as psg
 
 URL = "https://www.kimonoya-japan.net/"
 rs = req.get(URL)
@@ -7,5 +8,9 @@ pCont = bs(rs.content, "html.parser")
 
 results = pCont.find_all("div", class_="list_item_data")
 
+ly = [[]]
+
 for result in results:
 	print(result.prettify())
+
+psg.Window(title="results", layout=ly, margins=(200,200)).read()
